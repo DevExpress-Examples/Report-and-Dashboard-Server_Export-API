ebaff2dfd2833b3a168e4dd00c463bf94b3ca64d
Alexander <alexander.moiseev@devexpress.com>
2019-08-23 11:10:32 +0300

    export

--- Startup.cs
+++ Startup.cs
@@ -1,58 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Http;
 using Microsoft.Extensions.DependencyInjection;
-using System.Net.Http;
-using Newtonsoft.Json;
 
 namespace exportApi
 {
-    public interface IExportApiClient
-    {
-        Task<object> GetRandomActivity();
-    }
-
-    public class ExportApiClient : IExportApiClient
-    {
-        const string ExportDocumentStatus = "documents/95f71138eb424dc0a8c17da141496c93/export";
-
-        private readonly HttpClient _client;
-        private readonly string _remoteServiceBaseUrl;
-
-        public ExportApiClient(HttpClient httpClient)
-        {
-            httpClient.BaseAddress = new Uri("http://localhost:8192/api/");
-            // httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
-            // httpClient. Add("Content-type", "application/json");
-            _client = httpClient;
-        }
-
-        public async Task<object> GetRandomActivity()
-        {
-            var response = await _client.GetAsync(ExportDocumentStatus);
-
-            return JsonConvert.DeserializeObject<object>(null);
-        }
-
-        public async Task<object> GetCatalogItems(int page, int take,
-                                                   int? brand, int? type)
-        {
-
-
-            var responseString = await _client.GetStringAsync("uri");
-
-            var catalog = JsonConvert.DeserializeObject<object>(responseString);
-            return catalog;
-        }
-    }
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -71,9 +26,6 @@ namespace exportApi
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
-
-            // services.AddHttpClient< ExportApiClient>();
             services.AddHttpClient<IExportApiClient, ExportApiClient>()
                              //.SetHandlerLifetime(TimeSpan.FromSeconds(30)) // NOTE: 
                              ;
